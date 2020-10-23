8eea839f8805bb721c61b833c464ac584aacf4e5
Greg2fram2 <greg2fram@gmail.com>
2020-10-23 10:34:28 +0200

    first push, 2 files

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function Checkform(){
+    
+    if (document.getElementById("name").value == "")
+    {
+        alert('name empty');
+    }
+    if (document.getElementById("mail").value == "")
+    {
+        alert('mail empty');
+    }
+    if (document.getElementById("msg").value == "")
+    {
+        alert('msg empty');
+    }
+}
